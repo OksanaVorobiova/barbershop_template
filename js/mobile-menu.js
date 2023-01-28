35fc95cdac77f1551cbf4ec2433c063e1e1a8516
OksanaVorobiova <ov172608@gmail.com>
2023-01-28 23:39:28 +0200

    mobile menu, header and body scroll on opened menu are done

---
+++ js/mobile-menu.js
@@ -0,0 +1,38 @@
+const refs = {
+    openBtn: document.querySelector('.js-open-menu'),
+    closeBtn: document.querySelector('.menu__close'),
+    menuEl: document.querySelector('.menu'),
+}
+
+const { openBtn, closeBtn, menuEl } = refs;
+
+openBtn.addEventListener('click', onOpenBtnClick);
+
+
+function onOpenBtnClick(e) {
+    e.preventDefault();
+
+    if (!menuEl.classList.contains('is-open')) {
+        menuEl.classList.add('is-open');
+    }
+
+    document.body.style.overflow = 'hidden';
+    
+    openBtn.removeEventListener('click', onOpenBtnClick);
+    closeBtn.addEventListener('click', onCloseBtnClick);
+}
+
+function onCloseBtnClick(e) {
+    e.preventDefault();
+
+    if (menuEl.classList.contains('is-open')) {
+        menuEl.classList.remove('is-open');
+    }
+
+    document.body.style.overflow = 'visible';
+
+    closeBtn.removeEventListener('click', onCloseBtnClick);
+    openBtn.addEventListener('click', onOpenBtnClick);
+    
+}
+
