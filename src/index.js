871b1c79fcfd2676b04b1eaaa42243efa1155d6f
OksanaVorobiova <ov172608@gmail.com>
2023-02-15 23:23:14 +0200

    completed mobile version without animation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import menu from "./js/mobile-menu";
 import slider from "./js/slider";
 import fixedHeader from "./js/fixed-header";
-menu();
+
 slider();
 fixedHeader();
+menu();
--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,26 +1,26 @@
 export default (() => {
 
     const refs = {
-        openBtn: document.querySelector('.js-open-menu'),
+        openBtnArray: document.querySelectorAll('.js-open-menu'),
         closeBtn: document.querySelector('.menu__close'),
         menuEl: document.querySelector('.menu'),
     }
 
-    const { openBtn, closeBtn, menuEl } = refs;
+    const { openBtnArray, closeBtn, menuEl } = refs;
 
-    openBtn.addEventListener('click', onOpenBtnClick);
+    openBtnArray.forEach(openBtn => openBtn.addEventListener('click', onOpenBtnClick));
 
 
     function onOpenBtnClick(e) {
         e.preventDefault();
-
+        
         if (!menuEl.classList.contains('is-open')) {
             menuEl.classList.add('is-open');
         }
 
         document.body.style.overflow = 'hidden';
     
-        openBtn.removeEventListener('click', onOpenBtnClick);
+        openBtnArray.forEach(openBtn => openBtn.removeEventListener('click', onOpenBtnClick));
         closeBtn.addEventListener('click', onCloseBtnClick);
     }
 
@@ -34,7 +34,7 @@ export default (() => {
         document.body.style.overflow = 'visible';
 
         closeBtn.removeEventListener('click', onCloseBtnClick);
-        openBtn.addEventListener('click', onOpenBtnClick);
+        openBtnArray.forEach(openBtn => openBtn.addEventListener('click', onOpenBtnClick));
     
     }
 });
