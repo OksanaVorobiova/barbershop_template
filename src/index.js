c65acec50c003a120904ce0900d3b14f8060c165
OksanaVorobiova <ov172608@gmail.com>
2023-02-15 22:38:47 +0200

    fixed header added

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import menu from "./js/mobile-menu";
 import slider from "./js/slider";
+import fixedHeader from "./js/fixed-header";
 menu();
 slider();
+fixedHeader();
---
+++ src/js/fixed-header.js
@@ -0,0 +1,28 @@
+export default (() => {
+
+    const fixedHeader = document.querySelector('.fixed-header');
+    const isVisible = fixedHeader.classList.contains('visible');
+
+    
+    window.addEventListener('scroll', checkPosition);
+
+    function checkPosition(e) {
+        window.pageYOffset >= 580
+            ? showFixedHeader()
+            : hideFixedHeader();
+       // window.removeEventListener('scroll', checkPosition);
+    }
+
+
+    function showFixedHeader() {
+
+        if (!isVisible) fixedHeader.classList.add('visible');
+
+    }
+
+    function hideFixedHeader() {
+        if (fixedHeader.classList.contains('visible')) fixedHeader.classList.remove('visible');
+        
+    }
+
+})
