5b3ddbb383071b43f3d4c0b5c6b56ed01b4e6569
OksanaVorobiova <ov172608@gmail.com>
2023-02-19 00:02:09 +0200

    added navigation to header in tablet

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import menu from "./js/mobile-menu";
 import slider from "./js/slider";
 import fixedHeader from "./js/fixed-header";
+import responsiveFunctionsHeader from "./js/responsiveHeader";
 
 slider();
 fixedHeader();
 menu();
+responsiveFunctionsHeader();
---
+++ src/js/responsiveHeader.js
@@ -0,0 +1,43 @@
+export default (() => {
+
+    const refs = {
+        asideEl: document.querySelector('.aside'),
+        openMenuBtn: document.querySelector('.js-open-menu'),
+        mobileLogo: document.querySelector('.header__logo'),
+        navEl: document.querySelector('.nav'),
+        menuContacts: document.querySelector('.menu__contacts--wrapper'),
+        headerContainer: document.querySelector('.container--header'),
+    }
+
+    const { asideEl, navEl, menuContacts, headerContainer } = refs;
+    
+    window.addEventListener('resize', onResize);
+
+    toggleVisuallyHidden();
+
+    function onResize() {
+        toggleVisuallyHidden();
+    }
+
+    function toggleVisuallyHidden() {
+        window.matchMedia("(min-width: 768px)").matches
+            ? tablet()
+            : mobile();
+        
+    }
+
+    function mobile() {
+        asideEl.classList.add('visually-hidden');
+        
+    }
+
+    function tablet() {
+        asideEl.classList.remove('visually-hidden');
+        headerContainer.prepend(navEl, menuContacts); 
+    }
+
+
+
+
+
+});
